77dfe863b21db84f708e389481320871af5918fc
rajatsaha18 <editorrajatdiu@gmail.com>
2023-03-29 18:22:06 +0600

    create header, todo components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './MyComponents/Header';
+import Todos from './MyComponents/Todos';
+import Footer from './MyComponents/Footer';
 
 function App() {
   let myVariable = 'rajat';
   return (
     <>
     <Header/>
+    <Todos/>
+    <Footer/>
     </>
     
   );
